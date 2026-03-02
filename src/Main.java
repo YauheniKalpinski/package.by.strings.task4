38eef6d8986da5c301e6599306e6373ccf65e7e5
YauheniKalpinski <maksimludvug@gmail.com>
2026-03-02 14:52:47 +0300

    Добавил переовд строки в нижний регистр,переписал проверку на паллиндром,добавил метод для создания нового массива без несовподающего элемента

--- src/Main.java
+++ src/Main.java
@@ -11,56 +11,57 @@ public class Main {
     }
 
     public static boolean validPalindrome(String s) {
+        s = s.toLowerCase();
+        if (s.isEmpty()) {
+            System.out.println("Строка пустая и является палиндромом");
+            return true;
+        }
+
         char[] chars = s.toCharArray();
         boolean isLetters = isAllLetters(chars);
 
-        if (!s.isEmpty() && isLetters) {
-            int i;
-            int j = chars.length - 1;
-            int matchCount = 0;
+        if (isLetters) {
+            int left;
+            int right = chars.length - 1;
             int mismatchCount = 0;
-            int mismatchIndex = 0;
+            int mismatchIndex1 = 0;
+            int mismatchIndex2 = 0;
 
-            for (i = 0; i < j; i++, j--) {
-                if (chars[i] == chars[j]) {
-                    matchCount++;
-                } else {
-                    System.out.println("Найден не совпадающий символ под индексом: " + i);
+            for (left = 0; left < right; left++, right--) {
+                if (chars[left] != chars[right]) {
+                    System.out.printf("Первый несовпадающий символ под индексом: %d," +
+                            " второй под индексом: %d%n", left, right);
                     mismatchCount++;
-                    mismatchIndex = i;
+                    mismatchIndex1 = left;
+                    mismatchIndex2 = right;
+                    break;
                 }
             }
-
-            if (mismatchCount == 0){
+            if (mismatchCount == 0) {
                 System.out.println("Строка паллиндром");
                 return true;
             }
 
-            char[] newArray = new char[chars.length - 1];
+            char[] newArray1 = removeChar(chars, mismatchIndex1);
+            char[] newArray2 = removeChar(chars, mismatchIndex2);
 
-            for (i = 0; i < mismatchIndex; i++) {
-                newArray[i] = chars[i];
+            boolean resultNewArray1 = isPallindrome(newArray1);
+            boolean resultNewArray2 = isPallindrome(newArray2);
+
+            if (resultNewArray1 || resultNewArray2) {
+                return true;
+            } else {
+                return false;
             }
-
-            for (i = mismatchIndex + 1; i < chars.length; i++) {
-                newArray[i - 1] = chars[i];
-            }
-
-            for (i = 0; i < newArray.length; i++) {
-                System.out.print(newArray[i]);
-            }
-            return isPallindrome(newArray);
-
-        } else if (s.isEmpty()) {
-            System.out.println("Строка пустая это паллиндром");
+        } else {
+            System.out.println("В строке должны быть только буквы");
+            return false;
         }
-        return true;
     }
 
     public static boolean isAllLetters(char[] array) {
         for (char c : array) {
             if (!Character.isLetter(c)) {
-                System.out.println("В строке должны быть только буквы");
                 return false;
             }
         }
@@ -68,16 +69,20 @@ public class Main {
     }
 
     public static boolean isPallindrome(char[] array) {
-        String finalString = new String(array);
-        String reverse = new StringBuilder(finalString).reverse().toString();
-        if (finalString.equals(reverse)) {
-            System.out.println();
-            System.out.println("Строка является палиндромом");
-            return true;
-        } else {
-            System.out.println();
-            System.out.println("Строка не является палиндромом");
-            return false;
+        int right = array.length - 1;
+        for (int left = 0; left < right; left++, right--) {
+            if (array[left] != array[right]) {
+                return false;
+            }
         }
+        return true;
+    }
+
+    public static char[] removeChar(char[] chars, int indexToRemove) {
+        char[] newArray1 = new char[chars.length - 1];
+        System.arraycopy(chars, 0, newArray1, 0, indexToRemove);
+        System.arraycopy(chars, indexToRemove + 1, newArray1, indexToRemove,
+                chars.length - indexToRemove - 1);
+        return newArray1;
     }
 }
